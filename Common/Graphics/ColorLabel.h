c0035032ab45d72301fe70e42e1f3b3dcc1dc032
longepas <nobody@mail.net>
2017-09-03 15:43:48 +0500

    font

--- Common/Graphics/ColorLabel.h
+++ Common/Graphics/ColorLabel.h
@@ -9,7 +9,7 @@ public:
 	unsigned backColor;
 	wchar_t buffer[512];	
 	Gdiplus::RectF rect;
-	ColorLabel(): fontHeight(4), left(30), top(3), backColor(BACK_GROUND){buffer[0] = 0;}
+	ColorLabel(): fontHeight(7), left(30), top(3), backColor(BACK_GROUND){buffer[0] = 0;}
 	wchar_t *operator=(wchar_t *);
 	wchar_t *operator=(char *);
 	void Draw(Gdiplus::Graphics &);	
